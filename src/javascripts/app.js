f0d6e37b4a0a85f535268029bebc49aab70a59b5
LeVHoangduc <levhoangduc@gmail.com>
2023-08-08 15:09:25 +0700

    feat:initialize MVC

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -1,1 +1,18 @@
-// TODO: implement App Object
+import View from "./views/view";
+import Model from "./models/model";
+import Controller from "./controllers/controller";
+
+export class App {
+  /**
+   * Constructor off App object
+   */
+  constructor() {}
+
+  /**
+   * Function for starting the App
+   */
+  start() {
+    const controller = new Controller(new Model(), new View());
+    controller.init();
+  }
+}
