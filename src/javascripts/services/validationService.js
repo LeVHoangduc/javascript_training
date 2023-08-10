de89be1e500f1fd3c103b5637e28a89c020a8c0f
LeVHoangduc <levhoangduc@gmail.com>
2023-08-10 17:41:47 +0700

    feat:update validation service

--- src/javascripts/services/validationService.js
+++ src/javascripts/services/validationService.js
@@ -1,74 +1,131 @@
 import { MESSAGE, REGEX } from "../constants/constants";
-import { helpers } from "../helpers/helpers";
+
 class ValidationService {
   constructor() {}
 
   /**
-   * Method to validate form data
-   * @param {String} validation
-   * @param {HTMLElement} form
-   * @param {Object} object
-   * @returns {Boolean} object is validated
-   */
-  formValidator = (validation) => (form, object) => {
-    if (validation === "card") {
-      // Object to store field validation data
-      const cardFields = [
-        { name: "word", regex: REGEX.CONTENT, requiredMessage: MESSAGE.CONTENT_REQUIRED, invalidMessage: MESSAGE.INVALID_CONTENT },
-        { name: "type", regex: REGEX.CONTENT, requiredMessage: MESSAGE.CONTENT_REQUIRED, invalidMessage: MESSAGE.INVALID_CONTENT },
-        { name: "meaning", regex: REGEX.CONTENT, requiredMessage: MESSAGE.CONTENT_REQUIRED, invalidMessage: MESSAGE.INVALID_CONTENT },
-        { name: "typeMeaning", regex: REGEX.CONTENT, requiredMessage: MESSAGE.CONTENT_REQUIRED, invalidMessage: MESSAGE.INVALID_CONTENT },
-        { name: "description", regex: REGEX.CONTENT, requiredMessage: MESSAGE.CONTENT_REQUIRED, invalidMessage: MESSAGE.INVALID_CONTENT },
-        { name: "descriptionPhoto", regex: REGEX.IMAGE, requiredMessage: MESSAGE.CONTENT_REQUIRED, invalidMessage: MESSAGE.INVALID_IMAGE },
-      ];
-
-      return this.validationFields(form, cardFields, object);
-    }
-    if (validation === "user") {
-      const loginFields = [
-        { name: "email", regex: REGEX.EMAIL, requiredMessage: MESSAGE.EMAIL_REQUIRED, invalidMessage: MESSAGE.INVALID_EMAIL },
-        { name: "password", regex: REGEX.PASSWORD, requiredMessage: MESSAGE.PASSWORD_REQUIRED, invalidMessage: MESSAGE.INVALID_PASSWORD },
-      ];
-
-      return this.validationFields(form, loginFields, object);
-    } else {
-      return false;
-    }
-  };
-
-  /**
-   *
-   * @param {HTMLElement} form form will be validated
-   * @param {Object} fields fields validation for form
-   * @param {Object} object object is validated
+   * Function to check if the object is validated with the corresponding fields
+   * @param {Object} fields field validation for object
+   * @param {Object} object the validation object
    * @returns {Boolean} is object valid
    */
-  validationFields = (form, fields, object) => {
-    let isValid = true;
+  validationFields = (fields, object) => {
+    const fieldCheck = [];
     for (const field of fields) {
       const value = object[field.name];
       const isValidField = field.regex.test(value);
-      const inputEl = form[field.name]; // returns input tag here
-      const errorEl = form[field.name].nextElementSibling; // returns p tag here
 
       // Check if the field value is empty
       if (value.trim() === "") {
-        helpers.error.showError(inputEl, errorEl, field.requiredMessage);
-        isValid = false;
+        fieldCheck.push({
+          field: field.name,
+          isValid: false,
+          message: field.requiredMessage,
+        });
       }
-
       // Check if the field value matches the regex pattern
       else if (!isValidField) {
-        helpers.error.showError(inputEl, errorEl, field.invalidMessage);
-        isValid = false;
+        fieldCheck.push({
+          field: field.name,
+          isValid: false,
+          message: field.invalidMessage,
+        });
       }
-
       // If the field is valid, remove any error styling and message
       else {
-        helpers.error.clearError(inputEl, errorEl);
+        fieldCheck.push({
+          field: field.name,
+          isValid: true,
+          message: field.requiredMessage,
+        });
       }
     }
-    return isValid;
+
+    return fieldCheck;
+  };
+
+  /**
+   * Method to validate form data
+   * @param {String} validation  Type validation for which form
+   * user, card, language
+   * @param {HTMLElement} form
+   * user form, card form, language form
+   * @returns {Object/Boolean} returns object for deal with errors on UI
+   * or returns False if type validation is not defined
+   */
+  formValidator = (validation, form) => {
+    switch (validation) {
+      case "card":
+        // Get object for validation
+        const card = {
+          word: form[word],
+          type: form[type],
+          meaning: form[meaning],
+          description: form[description],
+          descriptionPhoto: form[descriptionPhoto],
+        };
+
+        // Object to store field validation data
+        const cardFields = [
+          {
+            name: "word",
+            regex: REGEX.CONTENT,
+            requiredMessage: MESSAGE.CONTENT_REQUIRED,
+            invalidMessage: MESSAGE.INVALID_CONTENT,
+          },
+          {
+            name: "type",
+            regex: REGEX.CONTENT,
+            requiredMessage: MESSAGE.CONTENT_REQUIRED,
+            invalidMessage: MESSAGE.INVALID_CONTENT,
+          },
+          {
+            name: "meaning",
+            regex: REGEX.CONTENT,
+            requiredMessage: MESSAGE.CONTENT_REQUIRED,
+            invalidMessage: MESSAGE.INVALID_CONTENT,
+          },
+          {
+            name: "description",
+            regex: REGEX.CONTENT,
+            requiredMessage: MESSAGE.CONTENT_REQUIRED,
+            invalidMessage: MESSAGE.INVALID_CONTENT,
+          },
+          {
+            name: "descriptionPhoto",
+            regex: REGEX.IMAGE,
+            requiredMessage: MESSAGE.CONTENT_REQUIRED,
+            invalidMessage: MESSAGE.INVALID_IMAGE,
+          },
+        ];
+
+        return this.validationFields(form, cardFields, card);
+      case "user":
+        // Get object for validation
+        const user = {
+          email: form.email.value,
+          password: form.password.value,
+        };
+
+        const loginFields = [
+          {
+            name: "email",
+            regex: REGEX.EMAIL,
+            requiredMessage: MESSAGE.EMAIL_REQUIRED,
+            invalidMessage: MESSAGE.INVALID_EMAIL,
+          },
+          {
+            name: "password",
+            regex: REGEX.PASSWORD,
+            requiredMessage: MESSAGE.PASSWORD_REQUIRED,
+            invalidMessage: MESSAGE.INVALID_PASSWORD,
+          },
+        ];
+
+        return this.validationFields(loginFields, user);
+      default:
+        false;
+    }
   };
 }
 
