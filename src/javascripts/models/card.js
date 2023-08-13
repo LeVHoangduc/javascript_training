e8702e018243325ae392a10269bfef473d07215f
LeVHoangduc <levhoangduc@gmail.com>
2023-08-13 19:49:30 +0700

    feat:update card model

--- src/javascripts/models/card.js
+++ src/javascripts/models/card.js
@@ -7,6 +7,16 @@ class Card {
   }
 
   /**
+   * Method to retrieve the list of card from the API.
+   * @returns {Promise<Object[]>} A promise that resolves with the list of card.
+   */
+  getCardList = async () => {
+    const data = await this.service.getList();
+
+    return data;
+  };
+
+  /**
    * MEthod to add a new card to the card list.
    * @param {Object} cardCurrent - The card object to be added.
    * @returns {Promise} A promise that resolves with the result of adding the card.
@@ -17,10 +27,16 @@ class Card {
     return addCard;
   };
 
+  /**
+   * Method to delete a card from the card list.
+   * @param {string} cardId - The ID of the card to be deleted.
+   */
+  deleteCard = async (cardId) => {
+    console.log("id of model is:", cardId);
+    await this.service.deleteItem(cardId);
+  };
+
   // TODO: editCard
-  // TODO: deleteCard
-  // TODO: getCardList
-  // TODO: getCardDetail
   // TODO: filterCard
 }
 
