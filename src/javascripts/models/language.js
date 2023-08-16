b7c913ccc45798186418de3a42317d727375863d
LeVHoangduc <levhoangduc@gmail.com>
2023-08-16 15:13:28 +0700

    feat: update language model

--- src/javascripts/models/language.js
+++ src/javascripts/models/language.js
@@ -3,20 +3,29 @@ import ApiService from "../services/apiService";
 class Language {
   constructor() {
     this.apiService = new ApiService("/languages");
-    this.languageList;
   }
 
-  //----- METHOD                   -----//
+  //-----  METHOD   -----//
 
   /**
-   * Method to get list of users from service
-   * @returns {Object[]} userList object
+   * Method to retrieve list of users from service
+   * @returns {Promise<Object[]>} - A promise that resolves with an array of language objects.
    */
-  getLanguageList = () => {
-    this.languageList = this.apiService.getList();
+  getLanguageList = () => this.apiService.getList();
 
-    return this.languageList;
-  };
+  /**
+   * Method to add a new card to the card list.
+   * @param {Object} languageData - The card object to be added.
+   * @returns {Promise<Boolean>} A promise that resolves with the result of adding the card.
+   */
+  addLanguage = (languageData) => this.apiService.postItem(languageData);
+
+  /**
+   * Method to delete a language based on its ID.
+   * @param {String} languageId - The ID of the language to be deleted.
+   * @returns {Promise<Boolean>} - A promise that resolves with the result of the deletion.
+   */
+  deleteLanguage = (languageId) => this.apiService.deleteItem(languageId);
 }
 
 export default Language;
