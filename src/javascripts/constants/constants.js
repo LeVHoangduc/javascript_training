57146cdc4a5de4bfb68964038938f32876898ddc
LeVHoangduc <levhoangduc@gmail.com>
2023-08-16 17:20:48 +0700

    feat: update constants

--- src/javascripts/constants/constants.js
+++ src/javascripts/constants/constants.js
@@ -2,13 +2,19 @@
  * Message for the form validator.
  */
 export const MESSAGE = {
-  USERNAME_REQUIRED: "The username is required",
-  INVALID_USERNAME: "The username is invalid",
+  EMAIL_REQUIRED: "The username is required",
+  INVALID_EMAIL: "The username is invalid",
+
   PASSWORD_REQUIRED: "The password is required",
   INVALID_PASSWORD: "The password is invalid",
+
   INVALID_CONTENT: "The content is invalid!",
   CONTENT_REQUIRED: "The content is required",
+
   INVALID_IMAGE: "The image is invalid",
+
+  CARD_CONFIRM_MESSAGE: "Do you want to delete this card",
+  LANGUAGE_CONFIRM_MESSAGE: "Do you want to delete this language",
 };
 
 /**
@@ -16,7 +22,9 @@ export const MESSAGE = {
  */
 export const ERROR_MESSAGE = {
   LOGIN_VALIDATION: "Login failed, please check your username or password",
+  INVALID_INFORMATION: "Something maybe is wrong in your information!",
   ADD_CARD: "Couldn't add card",
+  ADD_LANGUAGE: "Couldn't add language",
   SERVER_ERROR: "Server Error! Please try again later",
 };
 
@@ -26,6 +34,8 @@ export const ERROR_MESSAGE = {
 export const SUCCESS_MESSAGE = {
   LOGIN_VALIDATION: "Login successfully",
   ADD_CARD: "Add card successfully",
+  DELETE_CARD: "Delete card successfully",
+  DELETE_LANGUAGE: "Delete language successfully",
 };
 
 /**
@@ -34,6 +44,7 @@ export const SUCCESS_MESSAGE = {
 export const REGEX = {
   EMAIL: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
   PASSWORD: /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/,
+  LANGUAGE: /^[a-z]+$/,
   CONTENT: /^.{1,128}$/,
   IMAGE: /^https?:\/\/.*\.(jpg|jpeg|png|gif|bmp)$/,
 };
