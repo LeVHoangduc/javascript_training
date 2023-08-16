4f034f17f37764273aeb75df8f897d59c009924e
LeVHoangduc <levhoangduc@gmail.com>
2023-08-16 15:29:42 +0700

    feat: create card view

---
+++ src/javascripts/views/cardView.js
@@ -0,0 +1,45 @@
+import Template from "../templates/templates";
+
+class CardView {
+  constructor() {
+    this.cardListEl = document.querySelector(".card__list");
+  }
+
+  //----- RENDERING -----//
+
+  /**
+   * Render the list of cards based on data from Models filtered by the specified category.
+   * @param {Callback<Promise>} cardList - A function that returns a Promise of card data.
+   * @param {String} category - The category of cards to be rendered.
+   * @returns {Boolean} - Returns true when rendering is complete.
+   */
+  renderCardList = async (cardList, category) => {
+    // Show empty or loading effect
+    const cardListData = await cardList();
+
+    // Clear existing card elements before loading new data
+    this.cardListEl.innerHTML = "";
+
+    // Filter and render cards based on the specified category
+    const cards = cardListData.filter((card) => card.language === category);
+    cards.forEach((card) => {
+      this.renderCard(card);
+    });
+
+    return true;
+  };
+
+  /**
+   * Render an individual card using a provided card object.
+   * @param {Object} card - The card object containing information to be rendered.
+   */
+  renderCard = (card) => {
+    const cardTemplate = Template.renderCard(card);
+
+    if (this.cardListEl) {
+      this.cardListEl.innerHTML += cardTemplate;
+    }
+  };
+}
+
+export default CardView;
