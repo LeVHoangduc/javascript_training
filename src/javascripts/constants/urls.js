769149b634e46161ec82989a48d3d4010ca5b636
LeVHoangduc <levhoangduc@gmail.com>
2023-08-18 09:24:09 +0700

    feat:update

--- src/javascripts/constants/urls.js
+++ src/javascripts/constants/urls.js
@@ -1,6 +1,6 @@
 /**
  * API gateway url
  */
-const API_BASE_URL = "https://server-vocabulary.onrender.com/";
+const API_BASE_URL = "https://server-vocabulary.onrender.com";
 
 export default API_BASE_URL;
