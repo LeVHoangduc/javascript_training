ed49ee917505c99a571a0d4f3543f281da9a3813
LeVHoangduc <levhoangduc@gmail.com>
2023-08-21 17:07:08 +0700

    feat:create loading

--- src/javascripts/views/loginView.js
+++ src/javascripts/views/loginView.js
@@ -8,6 +8,8 @@ class LoginView {
     this.error = new Error();
     this.loginForm = document.querySelector(".login-form");
     this.buttonEl = document.querySelector(".login-form__button");
+
+    this.loadingEl = document.querySelector(".loading");
   }
 
   //----- EVENT HANDLER -----//
@@ -30,10 +32,14 @@ class LoginView {
       if (isValidation) {
         const isUSer = await isValidUSer(userCurrent);
 
-        // isUSer ? (window.location.href = "home.html") : alert(`${ERROR_MESSAGE.LOGIN_VALIDATION}`);
+        this.loginForm.classList.add("hide");
+        this.loadingEl.classList.add("open");
 
         if (isUSer) {
           this.resetForm();
+
+          this.loginForm.classList.remove("hide");
+          this.loadingEl.classList.remove("open");
 
           window.location.href = "home.html";
         }
