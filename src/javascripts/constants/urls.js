c7aef11eff10533633fefa1e74606fd23eb3d76e
LeVHoangduc <levhoangduc@gmail.com>
2023-08-24 10:27:04 +0700

    feat:restructure folder

--- src/javascripts/constants/urls.js
+++
@@ -1,6 +0,0 @@
-/**
- * API gateway url
- */
-const API_BASE_URL = "https://server-vocabulary.onrender.com";
-
-export default API_BASE_URL;
--- src/javascripts/main.js
+++
@@ -1,7 +0,0 @@
-import { App } from "./app";
-
-// Sure that scripts called after DOM loaded
-document.addEventListener("DOMContentLoaded", () => {
-  const myApp = new App();
-  myApp.start();
-});
